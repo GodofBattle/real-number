08d61c12970d795856b1d343d7c58cff38068006
Alex_Koh <godofbattle@gmail.com>
2025-07-11 23:53:44 +0900

    refactoring

--- app/api/lotto/generate/[idx]/route.ts
+++ app/api/lotto/generate/[idx]/route.ts
@@ -1,17 +1,15 @@
-
-import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
 type LottoIdx = {
     idx: string;
-}
+};
 
-const hasSeq = async (idx: number) => {
-    return (await prisma.lotto.count({ where: { id: idx } })) === 1;
-}
+// const hasSeq = async (idx: number) => {
+//     return (await prisma.lotto.count({ where: { id: idx } })) === 1;
+// };
 
-const GET = async (request: NextRequest, context: { params: LottoIdx }) => {
-    const idx: string = context.params.idx;
+const GET = async (request: NextRequest, { params }: { params: Promise<LottoIdx> }) => {
+    const { idx }: LottoIdx = await params;
 
     // if (await hasSeq(Number(idx))) {
 
@@ -22,11 +20,11 @@ const GET = async (request: NextRequest, context: { params: LottoIdx }) => {
     const response = await fetch(`https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=${idx}`, {
         credentials: 'same-origin',
         mode: 'no-cors',
-        cache: 'no-cache'
+        cache: 'no-cache',
     });
 
-    let result: ILottoDonghang = await response.json();
+    const result: ILottoDonghang = await response.json();
     return NextResponse.json({ result });
-}
+};
 
-export { GET }
+export { GET };
